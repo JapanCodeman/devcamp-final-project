bc93c028077fca3d9bd0d391abaa2fb919bc63f8
JapanCodeman <ryansurdick@MacBook-Pro.mshome.net>
2022-02-10 17:20:06 +0900

    Added some starter code to front end

--- finalProject/src/components/app.js
+++ finalProject/src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import './headerNavbar/headerNavbar';
+import HeaderNavbar from './headerNavbar/headerNavbar';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <HeaderNavbar />
+        <h1>Let's Go! Vocabulary Study System</h1>
+        <img className='OG-logo' src='../static/images/OG_logo.jpg' />
       </div>
     );
   }
---
+++ finalProject/src/components/headerNavbar/headerNavbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class HeaderNavbar extends Component {
+  render () {
+    return (
+      <div>
+        <div className='header-navbar'>
+          <h1>Onomichi Junior and Senior High School</h1>
+        </div>
+      </div>
+    );
+  }
+}
